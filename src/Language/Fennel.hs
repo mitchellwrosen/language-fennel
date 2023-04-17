2f8f43cd9b12b2fb5d122ff65788f0dce29ba5c7
Mitchell Rosen <mitchellwrosen@gmail.com>
2023-04-17 19:06:51 -0400

    parse comments

--- src/Language/Fennel.hs
+++ src/Language/Fennel.hs
@@ -8,98 +8,139 @@ import Data.Text qualified as Text
 import Data.Void (Void)
 import Text.Megaparsec qualified as Megaparsec
 import Text.Megaparsec.Char qualified as Megaparsec
-import Prelude hiding (exponent)
+import Prelude hiding (exponent, span)
 
-data FormF a
-  = HashFormF a
-  | ListFormF Delimiter [a]
-  | NumberFormF Text
-  | QuoteFormF a
-  | StringFormF Text
-  | SymbolFormF Text
-  | UnquoteFormF a
+-- | A piece of syntax: 0+ line comments (the first of which may be on the same line as the preceding syntax), plus a
+-- form (which may be empty only there is at least one comment).
+newtype Syntax
+  = Syntax (SyntaxF Syntax)
+  deriving stock (Show)
+
+data SyntaxF a = SyntaxF
+  { comments :: ![Comment],
+    span :: !Span,
+    form :: !(Form a)
+  }
+  deriving stock (Show)
+
+data Form a
+  = EmptyForm
+  | HashForm a
+  | ListForm Delimiter [a]
+  | NumberForm Text
+  | QuoteForm a
+  | StringForm Text
+  | SymbolForm Text
+  | UnquoteForm a
   deriving stock (Show)
 
 data Delimiter
-  = CurlyBracketDelimiter
-  | ParenDelimiter
-  | SquareBracketDelimiter
+  = CurlyBracket
+  | Paren
+  | SquareBracket
   deriving stock (Show)
 
-newtype Form
-  = Form (L (FormF Form))
+-- | A single line-comment.
+data Comment = Comment
+  { -- | The line the comment is on. It may appear after some other syntax on the same line.
+    line :: !Megaparsec.Pos,
+    -- | The comment text, minus the leading semicolon, but including any leading whitespace.
+    comment :: !Text
+  }
+  deriving stock (Show)
+
+-- | A span in a source file.
+data Span = Span
+  { start :: !Loc,
+    end :: !Loc
+  }
+  deriving stock (Show)
+
+-- | A location in a source file.
+data Loc = Loc
+  { line :: !Megaparsec.Pos,
+    col :: !Megaparsec.Pos
+  }
   deriving stock (Show)
 
 type Parser a =
   Megaparsec.Parsec Void Text a
 
-data L a = L
-  { startLine :: !Megaparsec.Pos,
-    startCol :: !Megaparsec.Pos,
-    endLine :: !Megaparsec.Pos,
-    endCol :: !Megaparsec.Pos,
-    value :: !a
-  }
-  deriving stock (Show)
+-- | A syntax parser.
+syntaxP :: Parser Syntax
+syntaxP =
+  Syntax <$> syntaxFP syntaxP
 
-formP :: Parser Form
-formP =
-  Form <$> formFP formP
-
-formFP :: forall a. Parser a -> Parser (L (FormF a))
-formFP parser = do
+syntaxFP :: Parser a -> Parser (SyntaxF a)
+syntaxFP parser = do
   whitespaceP
-  Megaparsec.SourcePos _ startLine startCol <- Megaparsec.getSourcePos
-  value <-
-    choice
-      [ listFormP '(' ')' ParenDelimiter,
-        listFormP '{' '}' CurlyBracketDelimiter,
-        listFormP '[' ']' SquareBracketDelimiter,
-        quoteFormP,
-        unquoteFormP,
-        hashFormP,
-        symbolFormP,
-        numberFormP,
-        stringFormP
-      ]
-  Megaparsec.SourcePos _ endLine endCol <- Megaparsec.getSourcePos
-  pure L {startLine, startCol, endLine, endCol, value}
+  comments <- many commentP
+  start <- getLoc
+  -- If there are any preceding comments, then allow parsing an empty form.
+  -- This allows us to parse the comments at the end of a list, as in:
+  --
+  --   (foo
+  --     bar
+  --     ; Hi I'm preceding nothing
+  --   )
+  form <-
+    if null comments
+      then formP parser
+      else formP parser <|> pure EmptyForm
+  end <- getLoc
+  let span = Span start end
+  pure SyntaxF {comments, span, form}
+
+-- | A form parser.
+formP :: forall a. Parser a -> Parser (Form a)
+formP parser =
+  choice
+    [ listFormP '(' ')' Paren,
+      listFormP '{' '}' CurlyBracket,
+      listFormP '[' ']' SquareBracket,
+      quoteFormP,
+      unquoteFormP,
+      hashFormP,
+      symbolFormP,
+      numberFormP,
+      stringFormP
+    ]
   where
-    listFormP :: Char -> Char -> Delimiter -> Parser (FormF a)
+    listFormP :: Char -> Char -> Delimiter -> Parser (Form a)
     listFormP ldelim rdelim delim = do
       _ <- Megaparsec.char ldelim
       whitespaceP
       forms <- many parser
       whitespaceP
       _ <- Megaparsec.char rdelim
-      pure (ListFormF delim forms)
+      pure (ListForm delim forms)
 
-    quoteFormP :: Parser (FormF a)
+    quoteFormP :: Parser (Form a)
     quoteFormP = do
       _ <- Megaparsec.char '`'
       form <- parser
-      pure (QuoteFormF form)
+      pure (QuoteForm form)
 
-    unquoteFormP :: Parser (FormF a)
+    unquoteFormP :: Parser (Form a)
     unquoteFormP = do
       _ <- Megaparsec.char ','
       form <- parser
-      pure (UnquoteFormF form)
+      pure (UnquoteForm form)
 
-    hashFormP :: Parser (FormF a)
+    hashFormP :: Parser (Form a)
     hashFormP = do
       _ <- Megaparsec.char '#'
       form <- parser
-      pure (HashFormF form)
+      pure (HashForm form)
 
-    symbolFormP :: Parser (FormF a)
+    symbolFormP :: Parser (Form a)
     symbolFormP = empty
 
-    numberFormP :: Parser (FormF a)
+    numberFormP :: Parser (Form a)
     numberFormP = do
       -- Why try: if we parse a sign (+/-), then no numbers, we don't want to have committed to parsing a number
       number <- Megaparsec.try (choice [hexNumberP, decimalNumberP])
-      pure (NumberFormF number)
+      pure (NumberForm number)
       where
         hexNumberP =
           fail "TODO: parse hex numbers"
@@ -141,8 +182,17 @@ formFP parser = do
               pure ""
             ]
 
-    stringFormP :: Parser (FormF a)
+    stringFormP :: Parser (Form a)
     stringFormP = empty
+
+-- | A comment parser.
+commentP :: Parser Comment
+commentP = do
+  _ <- Megaparsec.char ';'
+  Megaparsec.SourcePos _ line _ <- Megaparsec.getSourcePos
+  comment <- Megaparsec.takeWhileP (Just "comment") (/= '\n')
+  whitespaceP
+  pure Comment {line, comment}
 
 whitespaceP :: Parser ()
 whitespaceP =
@@ -150,3 +200,8 @@ whitespaceP =
     Megaparsec.takeWhileP
       (Just "whitespace")
       \c -> c == ' ' || c == '\n' || c == '\t' || c == '\r' || c == '\v' || c == '\f'
+
+getLoc :: Parser Loc
+getLoc = do
+  Megaparsec.SourcePos _ line col <- Megaparsec.getSourcePos
+  pure Loc {line, col}
